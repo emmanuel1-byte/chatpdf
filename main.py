82d6e2c696a0d518eda9b022c0c899d5194aa0c7
Emmanuel <emmanuelhilary9@gmail.com>
2025-06-04 14:24:12 +0100

    feat: added rate limitingand also adjusted the .tolm to suit the deployable version

--- main.py
+++ main.py
@@ -2,8 +2,17 @@ from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 from src.chat_pdf_api_service import rag, authentication
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
+from slowapi.middleware import SlowAPIMiddleware
 
+limiter = Limiter(key_func=get_remote_address, default_limits=["10/minute"])
 app = FastAPI()
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+
+app.add_middleware(SlowAPIMiddleware)
 
 app.add_middleware(
     CORSMiddleware,
@@ -15,7 +24,6 @@ app.add_middleware(
 
 app.include_router(authentication)
 app.include_router(rag)
-
 
 
 @app.get("/", tags=["Health"])
